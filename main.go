dc9f06e47ef55c0a254797a885dc5ba53fc2fbc9
wjojf <STPKBFF@yandex.ru>
2022-12-11 16:24:38 +0500

    Final

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"golang-jwt/controllers"
 	"golang-jwt/database"
+	"golang-jwt/middlewares"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -10,4 +14,22 @@ func main() {
 	database.Connect(AppConfig.ConnectionString)
 	database.Migrate()
 
+	router := initRouter()
+	router.Run(":8080")
+
+}
+
+func initRouter() *gin.Engine {
+	router := gin.Default()
+	api := router.Group("/api/v1/")
+	{
+		api.POST("/token", controllers.GenerateToken)
+		api.POST("/user/register", controllers.RegisterUser)
+		secured := api.Group("/secured").Use(middlewares.Auth())
+		{
+			secured.GET("/ping", controllers.Ping)
+		}
+	}
+
+	return router
 }
---
+++ middlewares/auth.go
@@ -0,0 +1,27 @@
+package middlewares
+
+import (
+	"golang-jwt/auth"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Auth() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		tokenString := ctx.GetHeader("Authorization")
+		if tokenString == "" {
+			ctx.JSON(401, gin.H{"error": "bad request"})
+			ctx.Abort()
+			return
+		}
+
+		err := auth.ValidateToken(tokenString)
+		if err != nil {
+			ctx.JSON(401, gin.H{"error": err.Error()})
+			ctx.Abort()
+			return
+		}
+
+		ctx.Next()
+	}
+}
